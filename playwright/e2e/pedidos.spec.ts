56403d766037834d4f926726b6e60ebdf354ee0a
shiteles <shiteles@gmail.com>
2026-02-09 22:24:33 -0300

    add validação de cores dos status

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -5,11 +5,6 @@ import { generateOrderCode } from '../support/helpers'
 
 test.describe('Consultar Pedido', () => {
 
-
-    test.beforeAll(async () => {
-        console.log('beforeAll: roda uma vez antes de todos os testes');
-    });
-
     test.beforeEach(async ({ page }) => {
         //Arrange
         await page.goto('http://localhost:5173/')
@@ -18,8 +13,6 @@ test.describe('Consultar Pedido', () => {
         await page.getByRole('link', { name: 'Consultar Pedido' }).click()
         await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
     });
-
-
 
     test('deve consultar um pedido aprovado', async ({ page }) => {
 
@@ -49,8 +42,9 @@ test.describe('Consultar Pedido', () => {
             - img
             - paragraph: Pedido
             - paragraph: ${order.number}
-            - img
-            - text: ${order.status}
+            - status:
+              - img
+              - text: ${order.status}
             - img "Velô Sprint"
             - paragraph: Modelo
             - paragraph: Velô Sprint
@@ -74,8 +68,15 @@ test.describe('Consultar Pedido', () => {
             - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
             `);
 
-    })
+        const statusBadge = page.getByRole('status').filter({ hasText: order.status })
 
+        await expect(statusBadge).toHaveClass(/bg-green-100/)
+        await expect(statusBadge).toHaveClass(/text-green-700/)
+
+        const statusIcon = statusBadge.locator('svg')
+        expect(statusIcon).toHaveClass(/lucide-circle-check-big/)
+
+    })
 
     test('deve consultar um pedido reprovado', async ({ page }) => {
 
@@ -105,8 +106,9 @@ test.describe('Consultar Pedido', () => {
             - img
             - paragraph: Pedido
             - paragraph: ${order.number}
-            - img
-            - text: ${order.status}
+            - status:
+              - img
+              - text: ${order.status}
             - img "Velô Sprint"
             - paragraph: Modelo
             - paragraph: Velô Sprint
@@ -129,6 +131,78 @@ test.describe('Consultar Pedido', () => {
             - paragraph: ${order.payment}
             - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
             `);
+
+        const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+
+        await expect(statusBadge).toHaveClass(/bg-red-100/)
+        await expect(statusBadge).toHaveClass(/text-red-700/)
+
+        const statusIcon = statusBadge.locator('svg')
+        expect(statusIcon).toHaveClass(/lucide-circle-x/)
+
+
+    })
+
+    test('deve consultar um pedido em analise', async ({ page }) => {
+
+        // Test Data
+
+        const order = {
+            number: 'VLO-YZFTUH',
+            status: 'EM_ANALISE',
+            color: 'Lunar White',
+            wheels: 'aero Wheels',
+            customer: {
+                name: 'Beyonce Knowles',
+                email: 'beyhive@email.com'
+            },
+            payment: 'À Vista'
+
+        }
+
+        //Act
+        await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+        // //Assert
+
+        await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order.number}
+            - status:
+              - img
+              - text: ${order.status}
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: ${order.color}
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: ${order.wheels}
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: ${order.customer.name}
+            - paragraph: Email
+            - paragraph: ${order.customer.email}
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: ${order.payment}
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+
+        const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+
+        await expect(statusBadge).toHaveClass(/bg-amber-100/)
+        await expect(statusBadge).toHaveClass(/text-amber-700/)
+
+        const statusIcon = statusBadge.locator('svg')
+        expect(statusIcon).toHaveClass(/lucide-clock/)
 
     })
 
